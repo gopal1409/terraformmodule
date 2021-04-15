019c7fda8354b0ef5998fb597dd3092df226f6b8
John Cena <John@gmail.com>
2021-04-15 00:45:58 -0600

    azure traffic manager

--- web/main.tf
+++ web/main.tf
@@ -38,3 +38,44 @@ module "location_us2e" {
     admin_password = data.azurerm_key_vault_secret.admin_password.value
     domain_name_label = var.domain_name_label
 }
+resource "azurerm_resource_group" "global_rg" {
+    name = "traffic-manager-rg"
+    location = "westus2"
+}
+
+resource "azurerm_traffic_manager_profile" "traffic_manager" {
+  name                   = "${var.resource_prefix}-traffic-manager"
+  resource_group_name    = azurerm_resource_group.global_rg.name
+  traffic_routing_method = "Weighted"
+
+  dns_config {
+    relative_name = var.domain_name_label
+    ttl           = 100
+  }
+
+  monitor_config {
+    protocol                     = "http"
+    port                         = 80
+    path                         = "/"
+    interval_in_seconds          = 30
+    timeout_in_seconds           = 9
+    tolerated_number_of_failures = 3
+  }
+
+}
+resource "azurerm_traffic_manager_endpoint" "traffic_manager_us2w" {
+  name                = "${var.resource_prefix}-us2w-endpoint"
+  resource_group_name = azurerm_resource_group.global_rg.name
+  profile_name        = azurerm_traffic_manager_profile.traffic_manager.name
+  target_resouece_id  = "module.location_us2w.web_server_lb_public_ip_id"
+  type                = "azureEndpoints"
+  weight              = 100
+}
+resource "azurerm_traffic_manager_endpoint" "traffic_manager_us2e" {
+  name                = "${var.resource_prefix}-us2e-endpoint"
+  resource_group_name = azurerm_resource_group.global_rg.name
+  profile_name        = azurerm_traffic_manager_profile.traffic_manager.name
+  target_resouece_id  = "module.location_us2w.web_server_lb_public_ip_id"
+  type                = "azureEndpoints"
+  weight              = 100
+}
